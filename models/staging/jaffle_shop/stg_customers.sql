479d963d953b7a1c9b5c6f615f25ba181a7de805
KAMESH KV <kameshkv1994@gmail.com>
2025-07-08 17:20:45 +0000

    commit

--- models/staging/jaffle_shop/stg_customers.sql
+++ models/staging/jaffle_shop/stg_customers.sql
@@ -1,11 +1,22 @@
-with customers as (
+with
+    customers as (
 
-    select
-        id as customer_id,
-        first_name,
-        last_name
+        select id as customer_id, first_name, last_name
 
-    from {{source('jaffle_shop','CUSTOMERS')}}
+        from {{ source("jaffle_shop", "CUSTOMERS") }}
 
-)
-select * from customers
+    ),
+    final_data as (
+        select
+            customers.customer_id,
+            customers.first_name,
+            customers.last_name,
+            case
+                when employees.employee_id = 1 then 'employee' else 'not employee'
+            end as is_employee
+        from customers
+        left outer join
+            {{ ref("employees") }} employees
+            on customers.customer_id = employees.customer_id
+    )
+select * from final_data
